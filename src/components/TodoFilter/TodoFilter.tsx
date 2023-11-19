872f8d81ecb76faab3e340f4f651d9f0449e5505
Artyom Volkov <artemvolk2099@yandex.ru>
2023-11-20 01:19:21 +0300

    Added a task counter

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,10 @@
+import { useAppSelector } from "../../hooks/hooks";
 
 const TodoFilter = () => {
+  const todos = useAppSelector((state) => state.todo.todos);
   return (
       <div className="flex justify-center mt-2">
-        <h3 className="text-xl">Список задач</h3>
+        <h3 className="text-xl">Список задач - {todos.length}</h3>
     </div>
   );
 };
